ee5d5246ec68b7fc54f32791f688ec00af4d471d
Shaikh Tashif <Shailhtashif87@gmail.com>
2025-12-17 23:55:32 +0530

    Add files via upload

---
+++ project4.py
@@ -0,0 +1,97 @@
+import json 
+import random
+import string 
+from pathlib import Path
+
+
+class Bank:
+    database =  'data.json'
+    data = []
+
+    try:
+        if Path(database).exists():
+            with open(database) as fs:
+                data = json.loads(fs.read())
+        else:
+            print("no such file exist")
+    except Exception as err:
+        print(f"an exception occured as {err}")
+
+    @classmethod
+    def __update(cls):
+        with open(Bank.database,'w') as fs:
+            fs.write(json.dumps(Bank.data))
+
+    @classmethod
+    def __accountgenrate(cls):
+        alpha = random.choices(string.ascii_letters,k = 3)
+        num = random.choices(string.digits,k =3)
+        spchar= random.choices("!@#$%^&*",k = 1)
+        id = alpha + num + spchar
+        random.shuffle(id)
+        return"".join(id)
+
+    def createaccount(self):
+        info = {
+            "name": input("Tell your name :-"),
+            "age" : int(input("Tell you age:")),
+            "E-mail": input("Tell your E-mail:"),
+            "pin": int(input("Tell your pin:")),
+            "accountNo." : Bank.__accountgenrate(),
+            "balabnce": 0 
+        }
+
+        if info['age'] < 18 or len(str(info['pin'])) !=4:
+            print("sorry you cannot create your account")
+        else:
+            print("account has been created succesfully")
+            for i in info:
+                print(f"{i} : {info[i]}")
+            print("please note down your account number")
+            
+            Bank.data.append(info)
+
+            Bank.__update()
+
+    def depositmoney(self):
+        accnumber = input("please tell your account number")
+        pin  = int(input("please tell your pin aswell"))
+
+        userdata = [i for i in Bank.data if['accountno.'] == accnumber and i['pin'] == pin]
+
+        if userdata == False:
+            print("sorry no data found")
+
+        else:
+            amount = int(input("how much you want to deposit"))
+            if amount > 10000 or amount < 0:
+                print("sorry the amount is too much you can deposit below 10000")
+
+            else:
+                print(userdata)
+                userdata[0]['balance'] += amount
+                Bank.__update()
+                print("Amount deposited succefully")
+
+
+user = Bank()
+print("press 1 for creating an account")
+print("press 2 for depositing the memory in the bank")
+print("press 3 wihdrawing the money ")
+print("press 4 for details ")
+print("press 5 for updating the details ")
+print("press 6 for deleting your account")
+
+check = int(input("tell your response:-"))
+
+if check == 1:
+    user.createaccount()
+
+if check ==2:
+    user.depositmoney()
+
+
+
+
+
+
